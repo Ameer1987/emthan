db84b947eb743328081d2be98454d22a83c63b1d
Ameer <ameer.atteya@rapid-develop.com>
2016-11-14 00:51:29 +0200

    tree javascript

--- src/AppBundle/Resources/public/js/tree.js
+++ src/AppBundle/Resources/public/js/tree.js
@@ -1,6 +1,14 @@
 jQuery(function ($) {
-    $('.term').on('click', function () {
-        $('.term').hide();
-        $(this).show();
+    $('li').on('click', function (event) {
+        event.stopPropagation();
+        $(this).parent().children().find('ul, li, a').hide();
+        $(this).parent().children().children('a').show();
+        $(this).show().children().show().children().show().children('a').show();
+    })
+
+    $(window).load(function () {
+        $('.tree').find('ul, li, a').hide();
+        $('.tree').show().children().show().children().show()
+                .children().show().children().show().children('a').show();
     })
 })
