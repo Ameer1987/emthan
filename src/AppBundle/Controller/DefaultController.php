cc86bae97b0fb449614add97118b0a94c91dfeee
Ameer <ameer.atteya@rapid-develop.com>
2016-11-13 18:54:58 +0200

    add first logic for the tree

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,16 +6,31 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        if ($request->getSession()->get('year_id') !== null) {
+            $year_id = $request->getSession()->get('year_id');
+            $year = $em->getRepository('AppBundle:Year')->findOneById($year_id);
+        } else {
+            $year = $em->getRepository('AppBundle:Year')->createQueryBuilder('y')
+                            ->orderBy('y.isDefault', 'DESC')
+                            ->setMaxResults(1)
+                            ->getQuery()->getOneOrNullResult();
+
+            $year_id = $year->getId();
+            $request->getSession()->set('year_id', $year_id);
+        }
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+                    'year' => $year,
         ]);
     }
+
 }
