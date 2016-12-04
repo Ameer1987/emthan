8b3eb18cba1e62bf0f8ec35f53a2f0593c1ab3c0
Magdy Yackoub <magdy.yackoub@mts-egypt.com>
2016-12-04 02:50:22 +0200

    Year Admin

---
+++ src/AppBundle/Admin/YearAdmin.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class YearAdmin extends AbstractAdmin {
+
+    protected function configureFormFields(FormMapper $formMapper) {
+        $formMapper
+                ->add('item', 'text')
+                ->add('createdBy', 'entity', array(
+                    'class' => 'AppBundle\Entity\User',
+                ))
+                ->add('createdAt', 'datetime')
+                ->add('updatedBy', 'entity', array(
+                    'class' => 'AppBundle\Entity\User',
+                ))
+                ->add('updatedAt', 'datetime')
+                ->add('isDefault')
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper) {
+        $datagridMapper->add('item')->add('createdBy')->add('createdAt')->add('updatedBy')->add('updatedAt')->add('isDefault');
+    }
+
+    protected function configureListFields(ListMapper $listMapper) {
+        $listMapper
+                ->addIdentifier('item')->addIdentifier('createdBy')->addIdentifier('createdAt')->addIdentifier('updatedBy')->addIdentifier('updatedAt')->addIdentifier('isDefault');
+    }
+
+}
