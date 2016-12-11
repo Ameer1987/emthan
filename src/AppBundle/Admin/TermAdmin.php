366dfb5576d4fcf17dac2454cfcec3814bb68eea
Magdy Yackoub <magdy.yackoub@mts-egypt.com>
2016-12-11 22:09:09 +0200

    Remove created by and at

--- src/AppBundle/Admin/TermAdmin.php
+++ src/AppBundle/Admin/TermAdmin.php
@@ -12,14 +12,6 @@ class TermAdmin extends AbstractAdmin {
     protected function configureFormFields(FormMapper $formMapper) {
         $formMapper
                 ->add('item', 'text')
-                ->add('createdBy', 'entity', array(
-                    'class' => 'AppBundle\Entity\FosUser',
-                ))
-                ->add('createdAt', 'datetime')
-                ->add('updatedBy', 'entity', array(
-                    'class' => 'AppBundle\Entity\FosUser',
-                ))
-                ->add('updatedAt', 'datetime')
                 ->add('year', 'entity', array(
                     'class' => 'AppBundle\Entity\Year',
                 ))
@@ -27,12 +19,12 @@ class TermAdmin extends AbstractAdmin {
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper) {
-        $datagridMapper->add('item')->add('createdBy')->add('createdAt')->add('updatedBy')->add('updatedAt')->add('year');
+        $datagridMapper->add('item')->add('year');
     }
 
     protected function configureListFields(ListMapper $listMapper) {
         $listMapper
-                ->addIdentifier('item')->addIdentifier('createdBy')->addIdentifier('createdAt')->addIdentifier('updatedBy')->addIdentifier('updatedAt')->addIdentifier('year');
+                ->addIdentifier('item')->addIdentifier('year');
     }
 
 }
