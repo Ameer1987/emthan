4d24745b99906947fdf5d87599e378aac9be1a84
Ameer <ameer.atteya@rapid-develop.com>
2016-12-12 14:45:21 +0200

    remove sub book container type controller

--- src/AppBundle/Form/SubBookTypeType.php
+++ src/AppBundle/Form/SubBookTypeType.php
@@ -13,7 +13,7 @@ class SubBookTypeType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('item')->add('subBookContainerType')        ;
+        $builder->add('item');
     }
     
     /**
