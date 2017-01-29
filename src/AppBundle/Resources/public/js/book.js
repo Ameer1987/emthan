35c7850736206484316fd1406482b6de446c64b1
Ameer Sameer <ext.ameer@gmail.com>
2017-01-29 07:32:14 +0200

    view book properties

---
+++ src/AppBundle/Resources/public/js/book.js
@@ -0,0 +1,14 @@
+jQuery(function ($) {
+    $('.book_properties').on('click', function (event) {
+        $('<div id="loading-overlay"><img src="/images/preloader.gif" />').prependTo($('body')).show();
+        var book_id = $(this).data('book-id');
+        $.get('/book/view_properties/' + book_id, null, function (data) {
+            $('#loading-overlay').remove();
+            $('#ajax_container').html(data);
+        });
+    })
+
+    $(window).load(function () {
+
+    })
+})
