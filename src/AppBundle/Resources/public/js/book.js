1adcc4565cfa331bca11bfda9a06b89811065d79
Ameer Sameer <ext.ameer@gmail.com>
2017-01-29 08:36:40 +0200

    view book units js

--- src/AppBundle/Resources/public/js/book.js
+++ src/AppBundle/Resources/public/js/book.js
@@ -8,6 +8,15 @@ jQuery(function ($) {
         });
     })
 
+    $('.book_units').on('click', function (event) {
+        $('<div id="loading-overlay"><img src="/images/preloader.gif" />').prependTo($('body')).show();
+        var book_id = $(this).data('book-id');
+        $.get('/book/view_units/' + book_id, null, function (data) {
+            $('#loading-overlay').remove();
+            $('#ajax_container').html(data);
+        });
+    })
+
     $(window).load(function () {
 
     })
