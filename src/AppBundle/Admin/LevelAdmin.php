da372f3335ee242a2d7bec41f07301746d6321d6
Ameer Sameer <ext.ameer@gmail.com>
2017-01-30 09:43:36 +0200

    add parent foreign key in the forms

--- src/AppBundle/Admin/LevelAdmin.php
+++ src/AppBundle/Admin/LevelAdmin.php
@@ -7,35 +7,32 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 
+class LevelAdmin extends AbstractAdmin {
 
-class LevelAdmin extends AbstractAdmin
-{
-    
-    protected function configureFormFields(FormMapper $formMapper)
-    {
+    protected function configureFormFields(FormMapper $formMapper) {
         $formMapper
                 ->add('item', 'text')
                 ->add('term', 'entity', array(
-                    'class' => 'AppBundle\Entity\Term'
+                    'class' => 'AppBundle\Entity\Term',
+                    'group_by' => function($val, $key, $index) {
+                        return $val->getYear();
+                    }
                 ))
-                ;
+        ;
     }
 
-    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
-    {
-        $datagridMapper->add('item')->add('term')        ;
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper) {
+        $datagridMapper->add('item')->add('term');
     }
 
-    protected function configureListFields(ListMapper $listMapper)
-    {
-        $listMapper->addIdentifier('item')->addIdentifier('term')        ;
+    protected function configureListFields(ListMapper $listMapper) {
+        $listMapper->addIdentifier('item')->addIdentifier('term');
     }
-    
+
     /**
      * {@inheritdoc}
      */
-    public function configureOptions(OptionsResolver $resolver)
-    {
+    public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'AppBundle\Entity\Level'
         ));
@@ -44,12 +41,8 @@ class LevelAdmin extends AbstractAdmin
     /**
      * {@inheritdoc}
      */
-    public function getBlockPrefix()
-    {
+    public function getBlockPrefix() {
         return 'appbundle_level';
     }
-    
-    
-    
 
 }
