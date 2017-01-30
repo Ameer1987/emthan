772080fb18d52fd2aa371d242b7878e6d77dd5e8
Ameer Sameer <ext.ameer@gmail.com>
2017-01-30 11:23:59 +0200

    add new book for category controller

--- src/AppBundle/Resources/public/js/book.js
+++ src/AppBundle/Resources/public/js/book.js
@@ -26,6 +26,15 @@ jQuery(function ($) {
         });
     })
 
+    $('.add-book').on('click', function (event) {
+        $('<div id="loading-overlay"><img src="/images/preloader.gif" />').prependTo($('body')).show();
+        var category_id = $(this).data('category-id');
+        $.get('/category/add_book/' + category_id, null, function (data) {
+            $('#loading-overlay').remove();
+            $('#ajax_container').html(data);
+        });
+    })
+
     $(window).load(function () {
 
     })
