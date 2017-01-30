951f5b0879b71b0e8466f993fb0e0c69bee25d1c
Ameer Sameer <ext.ameer@gmail.com>
2017-01-30 09:43:25 +0200

    add parent foreign key in the forms

--- src/AppBundle/Admin/SubjectAdmin.php
+++ src/AppBundle/Admin/SubjectAdmin.php
@@ -7,37 +7,34 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 
-class SubjectAdmin extends AbstractAdmin
-{
-    
-    
-    protected function configureFormFields(FormMapper $formMapper)
-    {
+class SubjectAdmin extends AbstractAdmin {
+
+    protected function configureFormFields(FormMapper $formMapper) {
         $formMapper
                 ->add('item', 'text')
-                
-                ->add('subLevel', 'entity' , array(
+                ->add('subLevel', 'entity', array(
                     'class' => 'AppBundle\Entity\SubLevel',
-                    'group_by' => 'level'
+                    'group_by' => function($val, $key, $index) {
+                        return $val->getLevel()->getTerm()->getYear()
+                                . ' - ' . $val->getLevel()->getTerm()
+                                . ' - ' . $val->getLevel();
+                    }
                 ))
-                ;
+        ;
     }
 
-    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
-    {
-        $datagridMapper->add('item')->add('subLevel')        ;
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper) {
+        $datagridMapper->add('item')->add('subLevel');
     }
 
-    protected function configureListFields(ListMapper $listMapper)
-    {
-        $listMapper->addIdentifier('item')->addIdentifier('subLevel')        ;
+    protected function configureListFields(ListMapper $listMapper) {
+        $listMapper->addIdentifier('item')->addIdentifier('subLevel');
     }
-    
+
     /**
      * {@inheritdoc}
      */
-    public function configureOptions(OptionsResolver $resolver)
-    {
+    public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'AppBundle\Entity\Subject'
         ));
@@ -46,11 +43,8 @@ class SubjectAdmin extends AbstractAdmin
     /**
      * {@inheritdoc}
      */
-    public function getBlockPrefix()
-    {
+    public function getBlockPrefix() {
         return 'appbundle_subject';
     }
 
-    
-    
 }
